a817fcce8c8305d568767a3bfafbbb34e93f54c0
Abdul Adil <abdul.adil@live.com>
2014-10-26 14:16:03 +0530

    Add

---
+++ src/com/connectica/emr/controller/LoginValidator.java
@@ -0,0 +1,17 @@
+package com.connectica.emr.controller;
+
+public class LoginValidator {
+	/**
+	 * Check_Sanitation() check for the invalid or wild card input to prevent attacks.
+	 */
+	String username,password;
+	public void Check_Sanitation(){
+		//Uses regular expression to check
+		
+	}
+	
+	public void ValidatorUserCredentials(){
+		//
+	}
+
+}
